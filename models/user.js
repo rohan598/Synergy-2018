7a0ac0a6d40a66dd4ab7cfd52481de55db8b9772
rohan598 <rohanwadhawan7@gmail.com>
2018-02-14 10:10:52 +0530

    added auth

---
+++ models/user.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose'),
+    bcrypt = require("bcrypt-nodejs");
+// define the schema for our user model
+
+var UserSchema = mongoose.Schema({
+    local            : {
+        email        : String,
+        password     : String
+    },
+    facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    }
+    });
+
+
+// methods ======================
+// generating a hash
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+UserSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model("user",UserSchema);
