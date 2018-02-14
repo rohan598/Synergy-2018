853fcbdfdc48ad0069be9195daccd2a7ea6f837a
TANUJ DRALL <Tanuj9043>
2018-02-14 10:14:17 +0530

    Routing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,16 +14,15 @@ mongoose.connection.once('open',()=>{
   console.log('connection estblished');
 }).on('error',error=>console.log(error));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -33,10 +32,9 @@ app.use(function (req, res, next) {
 });
 //
 // app.use('/signin',signinRoutes);
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use('/', appRoutes);
-
-
+app.use('/home', express.static('home'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -44,7 +42,7 @@ app.use(function (req, res, next) {
 });
 
 app.listen(process.env.PORT || 8080,process.env.IP,()=>{
-  console.log('server started');
+  console.log('Server running on port 8080');
 });
 
 // module.exports = app;
