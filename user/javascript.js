6930e9d6ec4059068ccdfac7e465ed182dc335bb
TANUJ DRALL <Tanuj9043>
2018-02-14 15:43:32 +0530

    Added user

---
+++ user/javascript.js
@@ -0,0 +1,46 @@
+function scrollto(id) {
+    $('html, body').stop().animate({
+        scrollTop: $('#'+id).offset().top-70
+    }, 500);
+}
+let DATA={};
+$(document).ready(function(){
+    window.scrollTo(0,0);
+    $.ajax('/getuser',function(data){
+        DATA=data;
+        setData();
+    });
+    $('#esend').click(function(){
+        var data = {
+            'from' : $('.uemail').val(),
+            'to' : $('.temail').val(),
+            'subject' : $('.cont').val(),
+            'content' : $('.esend').val()
+        };
+        $.ajax({
+            type : 'POST',
+            url : '/mail',
+            data : data,
+            success : (d)=>{
+                console.log(d);
+            }
+        });
+    });
+});
+
+function setData() {
+    $('.uname').html(`${DATA.uname}`);
+    $('.uemail').val()=DATA.uemail;
+    $('.tname').html(`${DATA.tname}`);
+    $('.temail').val()=DATA.temail;
+}
+
+$(document).on('scroll', function () {
+    $('.navbar').css('background-color', `rgba(0, 0, 0, ${($(document).scrollTop()+170) / 850})`);
+    let a = $(document).scrollTop() + $(window).height();
+    let b = $('#contact')[0].scrollHeight;
+    let c = $('body')[0].scrollHeight;
+    let x = (b - c + a) / b;
+    $('#contact').css('background-color', `rgba(0, 0, 0, ${x})`);
+    $('.bg').css('top', $(document).scrollTop());
+});
