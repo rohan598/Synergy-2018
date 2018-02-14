0cfefb0b0ba61effce704933b0e8aedff5306dad
rohan598 <rohanwadhawan7@gmail.com>
2018-02-14 10:51:43 +0530

    changes

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var path = require('path');
 // var favicon = require('serve-favicon');
 // var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+
+// var session = require("express-session");
+// var passport = require("passport");
+// var flash = require("connect-flash");
+
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var appRoutes = require('./routes/app');
@@ -20,9 +25,15 @@ mongoose.connection.once('open',()=>{
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
+// app.use(session({
+//     secret: "loda lassan",
+//     resave: false,
+//     saveUninitialized: false
+// }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+// app.use(flash());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
@@ -33,8 +44,9 @@ app.use(function (req, res, next) {
 });
 //
 // app.use('/signin',signinRoutes);
-app.use(express.static('public'));
+// app.use(express.static('public'));
 // app.use('/', appRoutes);
+
 
 
 
